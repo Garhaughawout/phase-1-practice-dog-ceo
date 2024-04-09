0ea0ff8359aa71ee5e2e76e65c3043e837aa7483
Garhaughawout <108907661+Garhaughawout@users.noreply.github.com>
2024-04-09 09:23:24 -0500

    Completed up to step 3
    up to step 3 need to do step 4

--- src/index.js
+++ src/index.js
@@ -1,1 +1,58 @@
-console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+fetch(imgUrl) 
+.then((resp) => resp.json())
+.then((data) => addImages(data))
+
+
+
+const dogImageContainer = document.querySelector("#dog-image-container")
+
+function addImages(data) {
+    data.message.forEach((image) => {
+        const img = document.createElement("img")
+        img.src = image
+        img.style.width = "200px"
+        img.style.height = "200px"
+        dogImageContainer.append(img)
+    })
+}
+
+const breedUrl = "https://dog.ceo/api/breeds/list/all";
+
+fetch(breedUrl)
+.then(resp => resp.json())
+.then(data => addBreeds(data))
+
+const dogBreeds = document.querySelector("#dog-breeds")
+
+function addBreeds(data) {
+    for (const breed in data.message) {
+        const li = document.createElement("li")
+        li.textContent = breed
+        dogBreeds.append(li)
+    }
+}
+
+const breedDropdown = document.querySelector("#breed-dropdown")
+
+breedDropdown.addEventListener("select", (e) => {
+    const letter = e.target.value
+    const allBreeds = document.querySelectorAll("li")
+    allBreeds.forEach((breed) => {
+        if (breed.textContent.startsWith(letter)) {
+            breed.style.display = ""
+        } else {
+            breed.style.display = "none"
+        }
+    })
+})
+
+
+
+
+
+
+
+
+
